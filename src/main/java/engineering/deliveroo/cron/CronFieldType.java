e6e16e49c8ec4c7b847ccdae900e09ec1d2b64d5
adamether <adamether@yandex-team.ru>
2020-03-15 23:52:10 +0300

    Remove brackets

--- src/main/java/engineering/deliveroo/cron/CronFieldType.java
+++ src/main/java/engineering/deliveroo/cron/CronFieldType.java
@@ -3,7 +3,7 @@ package engineering.deliveroo.cron;
 public enum CronFieldType {
     MINUTE(0, 59, 60),
     HOUR(0, 23, 24),
-    DAY_OF_MONTH(1, 31, 31) {},
+    DAY_OF_MONTH(1, 31, 31),
     MONTH(1, 12, 12),
     DAY_OF_WEEK(0, 6, 7);
 
